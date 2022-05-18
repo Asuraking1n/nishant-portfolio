110d48375084f69ef11e0f6d2206ced424b157bb
Asuraking1n <nishant99tiwari@gmail.com>
2022-05-18 09:05:32 -0700

    added portfolio

---
+++ src/data/BlogData.js
@@ -0,0 +1,44 @@
+export const Blogs = [
+    {
+        id:1,
+        name:"useEffect: A whole new mental model",
+        tags:["react","sideEffects","react lifecycle"],
+        date:"10 may, 2022",
+        imgSrc:"https://miro.medium.com/max/1400/1*WbPcXbngidrNUP3zz98XvA.gif",
+        link:"https://nishant99tiwari.medium.com/useeffect-a-whole-new-mental-model-8f1d01d41d04"
+    },
+    {
+        id:2,
+        name:"Transpiler VS Compiler",
+        tags:["javascript,transpilor,compilor"],
+        date:"8 jan, 2022",
+        imgSrc:"https://miro.medium.com/max/1400/1*q0rYUqIBDL2ScJUZalcVYw.png",
+        link:"https://nishant99tiwari.medium.com/transpiler-vs-compiler-2c138de85d01"
+    },
+    {
+        id:3,
+        name:"CSS Flex Boxing Guide",
+        tags:['css','flexbox'],
+        date:"10 may, 2022",
+        imgSrc:"https://miro.medium.com/max/1400/1*1yoqQO2PaKtmCskpz28Tkg.png",
+        link:"https://nishant99tiwari.medium.com/css-flex-boxing-guide-46f0a093e778"
+    },
+    {
+        id:4,
+        name:"What and Why if __name__ == “__main__” in Python",
+        tags:["python"],
+        date:"9 Jun, 2021",
+        imgSrc:"https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/640px-Python-logo-notext.svg.png",
+        link:"https://nishant99tiwari.medium.com/what-and-why-if-name-main-in-python-23bed7cac274"
+    },
+    {
+        id:5,
+        name:"Methodology: General approach to hunt bugs",
+        tags:["bugs","bounty"],
+        date:"1 April, 2021",
+        imgSrc:"https://miro.medium.com/max/1400/1*zlk4siAa1LnG0TtM-vjnfw.png",
+        link:"https://nishant99tiwari.medium.com/methodolgy-general-approach-to-hunt-bugs-8a75643f74a"
+    },
+    
+  
+] 
---
+++ src/data/WorkData.js
@@ -0,0 +1,50 @@
+export const Work = [
+    {
+        id:1,
+        name:"Amayra's Gallery",
+        description:"It is a video gallery build on top of the React JS, with MockBee as backend",
+        tags:["react","mockbee"],
+        
+        demo:"https://amayras-gallery.netlify.app/",
+        github:"https://github.com/Asuraking1n/amayras-vedio-gallery"
+    },
+    {
+        id:2,
+        name:"Amayra's Store",
+        description:"A E-commerce plateform build in ReactJS. With searching,filtering and many ecom functionalities.",
+        tags:["react","mockbee"],
+        
+        demo:"https://amayrastore.netlify.app/",
+        github:"https://github.com/Asuraking1n/amayra-shop-ecom"
+        
+    },
+    {
+        id:3,
+        name:"Amayra's Tracker",
+        description:"A habit tracker plateform build in ReactJS. With pomodoro, archive  and many tracker functionalities.",
+        tags:["react","api","mockbee"],
+        
+        demo:"https://amayrastracker.netlify.app/",
+        github:"https://github.com/Asuraking1n/amayrastracker"
+    },
+    {
+        id:4,
+        name:"Crome Extension",
+        description:"A cool looking extension to increase your productivity.",
+        tags:["react","crome"],
+        
+        demo:"https://chrome.google.com/webstore/detail/amayras-tab/pngdjolcchamoihdmcboabhpkmmgkcdi",
+        github:"https://github.com/Asuraking1n/amyrastab"
+       
+    },{
+        id:5,
+        name:"Wish UI",
+        description:"A todo app build using Redux-toolkit and framer-motion for smooth animations.",
+        tags:["html & css","CL"],
+        
+        demo:"https://wish-ui.netlify.app/",
+        github:"https://github.com/Asuraking1n/wish-ui"
+        
+    },
+
+] 
